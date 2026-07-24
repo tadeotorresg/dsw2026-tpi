4f73736d5b21d3f9e7ea8bdfe24b177bd2023423
Tadeo Torres Gauffin <torresgauffintadeo@gmail.com>
2026-07-24 00:18:37 -0300

    Agregar Dto, interfaz y servicio de especialidades

---
+++ Dsw2026Tpi.Application/Interfaces/ISpecialityService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dsw2026Tpi.Application.Dtos;
+using Dsw2026Tpi.Domain.Entities;
+
+
+namespace Dsw2026Tpi.Application.Interfaces
+{
+    public interface ISpecialityService
+    {
+        Task<Pagination<SpecialityModel.Response>>GetAllSpeciality(int pageSize, int pageIndex, string? name = null);
+        Task<SpecialityModel.Response> CreateSpeciality(SpecialityModel.Request Request);
+        Task<SpecialityModel.Response> UpdateSpeciality(Guid Id, SpecialityModel.Request Request);
+        Task DeleteSpeciality(Guid Id);
+
+
+    }
+}
