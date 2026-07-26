ef7110b9d1db7da96014b37b319e8b679bf1530d
lui <luisinadiazmontiveroh@gmail.com>
2026-07-26 00:29:37 -0300

    Modulo Availability

--- Dsw2026Tpi.CrossCutting/Exceptions/ConflictException.cs
+++ Dsw2026Tpi.CrossCutting/Exceptions/ConflictException.cs
@@ -5,8 +5,8 @@ namespace Dsw2026Tpi.CrossCutting.Exceptions;
 /// </summary>
 public class ConflictException : AppException
 {
-    public ConflictException(string errorCode, string message)
-        : base(errorCode, message)
+    public ConflictException(string message, string errorCode)
+        : base(message, errorCode)
     {
     }
 }
